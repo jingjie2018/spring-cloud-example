9c9ef6c1d3df9a170eea5d0305c9c3f4fb64969b
Jingjie <jingjie>
2019-04-30 07:49:01 +0800

    eureka

---
+++ eureka-client/src/main/java/com/isoft/springcloud/EurekaClientApplication.java
@@ -0,0 +1,20 @@
+package com.isoft.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * TODO 功能描述
+ *
+ * @author Jingjie
+ * @since 2019/4/30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
--- eureka-server/src/main/java/com/isoft/springcloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/isoft/springcloud/EurekaServerApplication.java
@@ -9,8 +9,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class App {
+public class EurekaServerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
