c15c4917562f590c543425ae8e9862b54f932c17
Jingjie <jingjie>
2019-04-30 23:01:45 +0800

    eureka-client

--- eureka-server/src/main/java/com/isoft/springcloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/isoft/springcloud/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Hello world!
+ * EurekaServerApplication
  */
 @SpringBootApplication
 @EnableEurekaServer
